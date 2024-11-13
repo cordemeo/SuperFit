3f6c6a0825142215e838a934f18c0a18590db5ef
cordemeo <shilinskaia@mail.ru>
2024-11-13 20:57:41 +0300

    added permissions

--- app/src/main/java/com/example/superfit01/services/YandexGPTService.kt
+++ app/src/main/java/com/example/superfit01/services/YandexGPTService.kt
@@ -1,11 +1,11 @@
 package com.example.superfit01.services
 
 
-import retrofit2.Response
+/*import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-/*interface YandexGPTService {
+interface YandexGPTService {
     @GET("api/v1/calculate_calories")
     suspend fun calculateCalories(@Query("product") product: String): Response<CalorieResponse>
 }
