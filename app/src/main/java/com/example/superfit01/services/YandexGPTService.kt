87d6c9c8a15c49f9c51a15364595e8d84ce7e1d3
cordemeo <shilinskaia@mail.ru>
2024-11-13 16:21:01 +0300

    Manual merge of source-branch into main

---
+++ app/src/main/java/com/example/superfit01/services/YandexGPTService.kt
@@ -0,0 +1,13 @@
+package com.example.superfit01.services
+
+
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface YandexGPTService {
+    @GET("api/v1/calculate_calories")
+    suspend fun calculateCalories(@Query("product") product: String): Response<CalorieResponse>
+}
+
+data class CalorieResponse(val calories: Int)
